f9e257b852aa1ccd7706c243136da8bcba0a473b
HemantPra389 <prajapatihemant768@gmail.com>
2021-11-16 01:28:37 +0530

    DataBinding Included

--- app/src/main/java/com/example/permutationandcombination/MainActivity.kt
+++ app/src/main/java/com/example/permutationandcombination/MainActivity.kt
@@ -4,35 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.permutationandcombination.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainViewModel: MainViewModel
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         mainViewModel=ViewModelProvider(this).get(MainViewModel::class.java)
 
         val_calculate.setOnClickListener {
-            if (val_n.text.isEmpty() || val_n.text.isEmpty()){
+            if (binding.valN.text.isEmpty() || binding.valR.text.isEmpty()){
                 Toast.makeText(this, "Empty Values", Toast.LENGTH_SHORT).show()
-                tv_val_com.text="0"
-                tv_val_per.text="0"
+                binding.tvValCom.text="0"
+                binding.tvValPer.text="0"
             }else{
                 mainViewModel.n=val_n.text.toString().toLong()
                 mainViewModel.r=val_r.text.toString().toLong()
-                if (val_n.text.toString().toLong()>20 || val_r.text.toString().toLong()>20){
+                if (binding.valN.text.toString().toLong()>20 || binding.valR.text.toString().toLong()>20){
                     Toast.makeText(this, "Not Valid", Toast.LENGTH_SHORT).show()
                 }else{
                     if (mainViewModel.n<mainViewModel.r){
                         Toast.makeText(this, "r cannot be greater than n", Toast.LENGTH_SHORT).show()
                     }else{
-                        tv_val_per.text= mainViewModel.setText_permutation(mainViewModel.n,mainViewModel.r)
-                        tv_val_com.text= mainViewModel.setText_combination(mainViewModel.n,mainViewModel.r)
+                        binding.tvValPer.text= mainViewModel.setText_permutation(mainViewModel.n,mainViewModel.r)
+                        binding.tvValCom.text= mainViewModel.setText_combination(mainViewModel.n,mainViewModel.r)
                     }
 
                 }
